10b2b32a1470943f530e741d6bf4a6346b6fae1a
Borna <80528615+borna-misetic@users.noreply.github.com>
2023-05-29 19:58:14 +0200

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rumble
+{
+    public partial class Main : Form
+    {
+        Random r = new Random();
+        List<string> messages = new List<string>();
+        public Main()
+        {
+            InitializeComponent();
+            messages.Add("Borna's Game Rumble Vol. 2 - Now in 2.5D!");
+            messages.Add("Borna's Game Rumble Vol. 2 - Also try Vol. 1");
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.IsMdiContainer = true;
+            this.Text = messages[r.Next(0,2)];
+            Startup start = new Startup();
+            start.MdiParent = this;
+            start.Show();
+            start.Dock = DockStyle.Fill;
+        }
+    }
+}
---
+++ Menu.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Rumble
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(347, 279);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lblTitle.Location = new System.Drawing.Point(212, 211);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(376, 29);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "BORNA\'S GAME RUMBLE VOL. 2";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
